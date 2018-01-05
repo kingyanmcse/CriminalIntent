d4a32dba87e429a30c3e683994ea4200894c11d7
yanj <mcse2003_yan@gmail.com>
2018-01-05 18:05:06 +0800

    new

--- app/src/main/java/com/king/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/king/android/criminalintent/CrimeListFragment.java
@@ -1,10 +1,27 @@
 package com.king.android.criminalintent;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by yanj on 2018/1/5.
  */
 
 public class CrimeListFragment extends Fragment {
+
+    private RecyclerView mCrimeRecyclerView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+        mCrimeRecyclerView = view.findViewById(R.id.crime_recycler_view);
+        mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return view;
+    }
 }
